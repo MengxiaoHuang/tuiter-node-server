759572a0c1a3b021fcca3a7484931726af2322f8
MengxiaoHuang <huang.meng@northeastern.edu>
2022-11-30 16:18:52 -0800

    fix for heroku deploy

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import cors from 'cors';
+import mongoose from "mongoose";
 
 const app = express();
 app.use(cors());
@@ -10,8 +11,7 @@ app.use(express.json());
 TuitsController(app);
 HelloController(app);
 UserController(app);
-app.listen(process.env.PORT || 4000);
-
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-    || 'mongodb+srv://mengxiao:mengxiao@cluster0.d96rtrh.mongodb.net/?retryWrites=true&w=majority'
+    || 'mongodb://localhost:27017/tuiter'
 mongoose.connect(CONNECTION_STRING);
+app.listen(process.env.PORT || 4000);
